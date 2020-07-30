57a86d114cdd9eac10b6d7bf5083020a9b4fcac5
linglingsyu <zzxcv741@hotmail.com>
2020-07-30 13:35:17 +0800

    完成會員管理功能

--- backend/admin.php
+++ backend/admin.php
@@ -25,15 +25,16 @@
       }else{
     ?>
     <button onclick="location.href='?do=edit_admin&id=<?= $ad['id'] ?>'">修改</button>
-    <button>刪除</button>
+    <button onclick="del('admin',<?= $ad['id'] ?>)">刪除</button>
     <?php
   }
     ?>
     </td>
   </tr>
   <?php
-    
   }
   ?>
 
 </table>
+
+<div class="ct"><button onclick="location.href='index.php'">返回</button></div>
--- backend/mem.php
+++ backend/mem.php
@@ -1,1 +1,28 @@
-會員
+<h2 class="ct">會員管理</h2>
+<table class="all ct">
+  <tr class="tt">
+    <td>姓名</td>
+    <td>會員帳號</td>
+    <td>註冊日期</td>
+    <td>管理</td>
+  </tr>
+  <?php
+  $mems = $Member->all();
+  foreach ($mems as $mem) {
+
+  ?>
+    <tr>
+      <td class="pp"><?= $mem['name'] ?></td>
+      <td class="pp"><?= $mem['acc'] ?></td>
+      <td class="pp"><?= $mem['regdate'] ?></td>
+      <td class="pp">
+        <button onclick="location.href='?do=edit_mem&id=<?= $mem['id'] ?>'">修改</button>
+        <button onclick="del('member',<?= $mem['id'] ?>)">刪除</button> </td>
+    </tr>
+  <?php
+  }
+  ?>
+
+</table>
+
+<div class="ct"><button onclick="location.href='index.php'">返回</button></div>
